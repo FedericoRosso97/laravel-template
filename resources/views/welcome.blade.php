2e6e596ffa6ab1318048d980e5422b715e7ead20
FedericoRosso97 <federicorosso97@gmail.com>
2023-07-20 16:05:05 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title','Welcome')
+@section('main')
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <p>ciao</p>
+        </div>
+        <div class="col-6">
+            <p>ciao</p>
+        </div>
+    </div>
+</div>
+  <h1>
+    Hello World
+  </h1>
+@endsection
